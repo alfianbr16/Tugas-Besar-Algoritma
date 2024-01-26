71150f6fc0e1502cea717a66a9a85ce6c8c11dee
alfianbr16 <alfianbr84@gmail.com>
2024-01-26 08:37:07 +0700

    push

---
+++ TUGAS BESAR/Blocks.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Blocks here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Blocks extends Actor
+{
+    /**
+     * Act - do whatever the Blocks wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public Blocks()
+    {
+      getImage().setColor(Color.BLUE);
+      getImage().fillRect(0,0, 60, 20);
+    }
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
